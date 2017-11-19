003b11b97c6e01341cf474aa822430e8e9519c00
Martin Sandström <martinsandstroms@gmail.com>
2017-11-19 22:22:26 +1100

    First commit

---
+++ src/components/PersonCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+export const PersonCard = props => {
+    return (
+        <div className="swipe-container mx-auto">
+            <div className="swipe-card mx-auto">
+                <img width="100%" height="300" className="swipe-image" src={props.src} alt="person" />
+                <h1>{props.name}</h1>
+            </div>
+            <div className="row swipe-buttons-container">
+                <div className="col-xs-6 mx-auto">
+                    <Button color="danger" className="swipe-button" onClick={() => props.handleSwipe({ direction: 2 })}>
+                        <i className="fa fa-frown-o fa-2x" aria-hidden="true"></i>
+                    </Button>
+                </div>
+                <div className="col-xs-6 mx-auto">
+                    <Button color="success" className="swipe-button" onClick={() => props.handleSwipe({ direction: 4 })}>
+                        <i className="fa fa-check-circle fa-2x" aria-hidden="true"></i>
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
